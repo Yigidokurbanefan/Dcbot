93438726f988793e76c90ad05761420d0920a34e
Yigidokurbanefan <151950556+Yigidokurbanefan@users.noreply.github.com>
2024-09-20 23:04:20 +0300

    Update Dcbotkomut.py

--- Dcbotkomut.py
+++ Dcbotkomut.py
@@ -4,6 +4,7 @@ import random
 import os
 import requests
 import dcbotimg
+import gemini
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -25,6 +26,11 @@ async def MyBot(ctx, message, message1= "", message2="", message3 = "", message4
     i = random.randint(1,10)
     j = random.randint(1,10)
     k = random.randint(1,10)
+    skor1 = random.randint(1,5)
+    skor2 = random.randint(1,5)
+    futbolcular = ["Lionel Messi", "Cristiano Ronaldo", "Diego Maradona", "Pele", "Robert Lewandowski", "Karim Benzema", "Sergio Ramos", "Paolo Maldini", "Giangluigi Buffon", "Iker Cassillas"]
+    cevap = random.choice(futbolcular)
+    mesaj = "Bir şey", "Ciddi bir şey", "Çok ciddi bir şey"
     ulke = "Türkiye'nin Başkenti Ankara'dır.","İspanya'nın Başkenti Madrid'dir.","Rusya'nın Başkenti Moskova'dır."
     if message == "Nasılsın?":
         await ctx.send("İyi Senden?")
@@ -101,7 +107,7 @@ async def MyBot(ctx, message, message1= "", message2="", message3 = "", message4
                 kullanici_tahmini3 = tahminmesaji3.content
                 try:
                     kullanici_tahmini3 = float(kullanici_tahmini3)
-                    if kullanici_tahmini3 == (h/i):
+                    if kullanici_tahmini3 == (j/k):
                         await ctx.send("Tebrikler!")
                     else:
                         await ctx.send("Yanlış")
@@ -109,9 +115,166 @@ async def MyBot(ctx, message, message1= "", message2="", message3 = "", message4
                     await ctx.send("Lütfen Sayı Gönderin")
     elif message == "Ülke-Başkent":
         await ctx.send(random.choice(ulke))
+    elif message == "Rastgele":
+        if message1 == "Bilgi":
+            await ctx.send(random.choice(mesaj))
+    elif message == "Telefon":
+        if message1 == "Bakma":
+            if message2 == "Süresi":
+                await ctx.send("Kaç Saat Telefona Bakıyorsun ?")
+                alinan_mesaj = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                kullanici_mesaj = alinan_mesaj.content
+                if int(kullanici_mesaj) <= 2 :
+                    await ctx.send("Bağımlı değilsin.")
+                elif int(kullanici_mesaj) > 2 and int(kullanici_mesaj) <= 4 :
+                    await ctx.send("Dikkat et.")
+                elif int(kullanici_mesaj) >= 5 :
+                    await ctx.send("SEN TAM BİR BAĞIMLISIN.")
+    elif message == "Maç":
+        if message1 == "Skoru":
+            if message2 == "Simulasyonu":
+                await ctx.send("...")
+                time.sleep(3)
+                await ctx.send("Birinci takımın ismini girin")
+                takim_mesaj = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                ilktakim_mesaj = takim_mesaj.content
+                await ctx.send("...")
+                time.sleep(3)
+                await ctx.send("İkinci takımın ismini girin")
+                takim_mesaj1 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                ikincitakim_mesaj = takim_mesaj1.content
+                await ctx.send("...")
+                time.sleep(3)
+                if ilktakim_mesaj == "Galatasaray" or "Beşiktaş" and ikincitakim_mesaj == "Galatasaray" or "Beşiktaş":
+                    await ctx.send("Maç sonucu Beşiktaş5-0Galatasaray")
+                    await ctx.send("Maçı Beşiktaş kazandı!")
+                else:
+                    await ctx.send(f"{ilktakim_mesaj}{skor1}-{skor2}{ikincitakim_mesaj}")
+                    if skor1 > skor2:
+                        await ctx.send(f"Maçı {ilktakim_mesaj} kazandı!")
+                    elif skor1 == skor2:
+                        await ctx.send("Maç berabere!")
+                    else:
+                        await ctx.send(f"Maçı {ikincitakim_mesaj} kazandı!")
+    elif message == "Üs":
+        if message1 == "Hesaplayıcı":
+            baslangic = []
+            await ctx.send("...")
+            time.sleep(3)
+            await ctx.send("Kuvveti alınacak sayıyı girin.")
+            sayi_mesaj = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+            taban_mesaj = sayi_mesaj.content
+            baslangic.append(int(taban_mesaj))
+            await ctx.send("...")
+            time.sleep(3)
+            await ctx.send("Kuvveti girin.")
+            sayi_mesaj1 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+            kuvvet_mesaj = sayi_mesaj1.content
+            for i in range(int(kuvvet_mesaj) - 1):
+                taban_mesaj = int(taban_mesaj)*baslangic[0]
+            await ctx.send("...")
+            time.sleep(3)
+            await ctx.send(f"Sonuç {taban_mesaj}")
+    elif message == "Kök":
+        if message1 == "İçine":
+            if message2 == "Tam":
+                if message3 == "Kare": 
+                    if message4 == "Alma":
+                        await ctx.send("...")
+                        time.sleep(3)
+                        await ctx.send("Tam kare sayıyı girin.")
+                        tam_mesaj = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                        sonuc_mesaj = int(tam_mesaj.content)
+                        if sonuc_mesaj**(1/2) == int(sonuc_mesaj**(1/2)):
+                            await ctx.send(f"Sonuç {int(sonuc_mesaj**(1/2))}")
+                        else:
+                            await ctx.send("Bu bir tam kare değil.")
+    elif message == "Futbolcu":
+        if message1 == "Tahmin":
+            if message2 == "Etme":
+                if cevap == "Lionel Messi":
+                    await ctx.send("En iyisi, Arjantin, Sağ Kanat")
+                    gercekcevap = await bot.wait_for("message", check=lambda m: m.author == ctx.author) 
+                    yenicevap = gercekcevap.content
+                    if yenicevap == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Cristiano Ronaldo":
+                    await ctx.send("Gol, Portekiz, Sol Kanat")
+                    gercekcevap1 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap1 = gercekcevap1.content
+                    if yenicevap1 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Diego Maradona":
+                    await ctx.send("El, Arjantin, 1986")
+                    gercekcevap2 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap2 = gercekcevap2.content
+                    if yenicevap2 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Pele":
+                    await ctx.send("Dünya Kupası, Brezilya, Edson")
+                    gercekcevap3 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap3 = gercekcevap3.content
+                    if yenicevap3 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Robert Lewandowski":
+                    await ctx.send("Goalski, Polonya, Santrafor")
+                    gercekcevap4 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap4 = gercekcevap4.content
+                    if yenicevap4 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Karim Benzema":
+                    await ctx.send("Bandaj, Fransa, Gölge")
+                    gercekcevap5 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap5 = gercekcevap5.content
+                    if yenicevap5 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Sergio Ramos":
+                    await ctx.send("Kart, İspanya, Stoper")
+                    gercekcevap6 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap6 = gercekcevap6.content
+                    if yenicevap6 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Paolo Maldini":
+                    await ctx.send("Milan, İtalya, Stoper")
+                    gercekcevap7 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap7 = gercekcevap7.content
+                    if yenicevap7 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Gianluigi Buffon":
+                    await ctx.send("45, İtalya, Kaleci")
+                    gercekcevap8 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap8 = gercekcevap8.content
+                    if yenicevap8 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
+                elif cevap == "Iker Cassillas":
+                    await ctx.send("Kaptan, İspanya, Kaleci")
+                    gercekcevap9 = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+                    yenicevap9 = gercekcevap9.content
+                    if yenicevap9 == cevap:
+                        await ctx.send("Doğru")
+                    else:
+                        await ctx.send("Yanlış")
 
 
-
+                     
 
 @bot.command()
 async def BenimBot(ctx):
@@ -195,10 +358,76 @@ async def EBOB(ctx, *args):
             return
         bolunenler.remove(max(bolunenler))
 
+
+
+def EKOK_Hazirlik(numbers):
+    def gcd(x, y):
+        while y:
+            x, y = y, x % y
+        return x
+    lcm = numbers[0]
+    for number in numbers[1:]:
+        lcm = abs(lcm * number) // gcd(lcm, number)
+    return lcm
+
+@bot.command()
+async def EKOK(ctx, *args):
+    if len(args) < 2:
+        await ctx.send("Lütfen en az iki sayı girin.")
+        return
+
+    try:
+        numbers = [int(arg) for arg in args]
+    except ValueError:
+        await ctx.send("Lütfen geçerli sayılar girin.")
+        return
+
+    result = EKOK_Hazirlik(numbers)
+    await ctx.send(f"Girdiğiniz sayıların EKOK'u: {result}")
+
+
+
+@bot.command()
+async def Secim(ctx, * , secim):
+    eleman = secim.split("-")
+    await ctx.send(random.choice(eleman))
+
+@bot.command()
+async def Gemini(ctx, * , bildir):
+    gemini.geminim(bildir)
+    gonder = "output.txt"
+    await ctx.send(file=discord.File(gonder))
+    
+
+
 @bot.command()
 async def Rastgele(ctx):
     komut = random.randint(1,100)
     await ctx.send(komut)
+
+@bot.command()
+async def RastgeleCıkartma(ctx):
+    komut1 = random.randint(1,100)
+    komut2 = random.randint(1,100)
+    await ctx.send(komut1 - komut2)
+
+@bot.command()
+async def RastgeleToplama(ctx):
+    komut3 = random.randint(1,100)
+    komut4 = random.randint(1,100)
+    await ctx.send(komut3 + komut4)
+
+@bot.command()
+async def RastgeleCarpma(ctx):
+    komut5 = random.randint(1,100)
+    komut6 = random.randint(1,100)
+    await ctx.send(komut5 * komut6)
+
+@bot.command()
+async def RastgeleBolme(ctx):
+    komut7 = random.randint(1,100)
+    komut8 = random.randint(1,100)
+    await ctx.send(komut7 / komut8)
 
 
 
